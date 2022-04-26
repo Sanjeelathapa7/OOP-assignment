2a049f18db87570d78150aafeacf8da4832da142
Sanjeelathapa7 <sanjeelathapamagar1234@gmail.com>
2022-04-26 11:53:00 +0545

    Changes

---
+++ src/Salary.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the salary: ");
+        int salary = scanner.nextInt();
+        if (salary <= 10000) {
+            System.out.println("HRA=20%\nDA=80%");
+        } else if (salary <= 20000) {
+            System.out.println("HRA=25%\nDA=90%");
+        } else {
+            System.out.println("HRA=30%\nDA=95%");
+        }
+    }
+}
